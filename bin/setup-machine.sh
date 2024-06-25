23147ae49cce389bd7e6bb0fac80db442a9e569b
Jason Hyde <github@2bad.me>
2024-06-25 03:36:55 +0200

    [conf] update docker install step

--- bin/setup-machine.sh
+++ bin/setup-machine.sh
@@ -64,7 +64,7 @@ function install_docker() {
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update -y
-    sudo apt-get install -y docker-ce
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     # Ref. https://github.com/microsoft/WSL/discussions/4872
     sudo touch /etc/fstab
     sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
