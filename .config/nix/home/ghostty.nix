08a9328556648212eef22bde2ba537cf055ce71d
Jason Hyde <jason@xliberate.com>
2025-12-08 00:35:57 +0100

    [conf] add ghostty config

---
+++ .config/nix/home/ghostty.nix
@@ -0,0 +1,17 @@
+_: {
+  xdg.configFile."ghostty/config".text = ''
+    # theme
+    theme = Monokai Remastered
+
+    # fonts
+    font-family = MesloLGS Nerd Font
+    font-size = 14
+
+    shell-integration = zsh
+    macos-option-as-alt = true
+    mouse-hide-while-typing = true
+    adjust-cursor-thickness = 2
+    window-colorspace = display-p3
+    keybind = shift+enter=text:"\n"
+  '';
+}
