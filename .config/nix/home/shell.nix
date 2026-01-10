6a4f7f7a9b775ba5b3b0414522239807a12d315a
Jason Hyde <jason@xliberate.com>
2026-01-11 00:53:45 +0100

    [conf] let z4h manage zsh config instead of nix

--- .config/nix/home/shell.nix
+++ .config/nix/home/shell.nix
@@ -1,14 +1,3 @@
 _: {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      la = "ls -la";
-      ".." = "cd ..";
-      "nix-switch" = "sudo darwin-rebuild switch --flake ~/.config/nix";
-    };
-  };
+  programs.zsh.enable = false;
 }
